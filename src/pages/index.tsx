193a595af72e00d993a39f56588c715d4e0a262d
bong <jungcome7@gmail.com>
2024-03-17 12:15:39 +0900

    feat: add get-user logic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,27 +1,55 @@
 import Button from '@components/Button';
 import axios from 'axios';
+import { useAtomValue } from 'jotai';
 import { useState } from 'react';
 import { useQuery } from 'react-query';
+import { jwtAtom } from 'src/atoms';
+import { userIdAtom } from 'src/atoms/user';
+import { css } from 'styled-system/css';
 
 export default function Page() {
   const [isUserInfoVisible, setIsUserInfoVisible] = useState(false);
+  const jwt = useAtomValue(jwtAtom);
+  const userId = useAtomValue(userIdAtom);
 
-  const { data } = useQuery(
+  const { data } = useQuery<{
+    data: { id: string; email: string; name: string };
+  }>(
     'user',
-    async () => {
-      axios.get('http://localhost:3001/users');
-    },
+    async () =>
+      axios.get(`http://localhost:3001/users/${userId}`, {
+        headers: {
+          Authorization: `Bearer ${jwt}`,
+        },
+      }),
     { enabled: isUserInfoVisible }
   );
-
-  console.log({ data });
 
   return (
     <>
       <h1>Main page</h1>
-      <Button color="gray" onClick={() => setIsUserInfoVisible(true)}>
-        Get User Info
+      <Button
+        color="gray"
+        onClick={() => setIsUserInfoVisible(!isUserInfoVisible)}
+      >
+        {isUserInfoVisible ? 'Hide User Info' : 'Show User Info'}
       </Button>
+      {isUserInfoVisible && (
+        <div className={css({ marginTop: '20' })}>
+          <h2>
+            <b>User Info</b>
+          </h2>
+          <p>
+            <b>Name:</b> {data?.data.name}
+          </p>
+          <p>
+            <b>Email:</b> {data?.data.email}
+          </p>
+          <p>
+            <b>Id:</b> {data?.data.id}
+          </p>
+        </div>
+      )}
     </>
   );
 }
--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,13 +1,18 @@
 import Button from '@components/Button';
 import axios, { AxiosError } from 'axios';
+import { useSetAtom } from 'jotai';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import { useMutation } from 'react-query';
+import { jwtAtom } from 'src/atoms';
+import { userIdAtom } from 'src/atoms/user';
 import { vstack } from 'styled-system/patterns';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const setJwt = useSetAtom(jwtAtom);
+  const setUserId = useSetAtom(userIdAtom);
 
   const router = useRouter();
 
@@ -15,12 +20,13 @@ function Login() {
     mutationFn: (payload: { email: string; password: string }) => {
       return axios.post('http://localhost:3001/users/login', payload);
     },
-    onSuccess: () => {
+    onSuccess: data => {
+      setJwt(data.data.jwt);
+      setUserId(data.data.id);
+
       router.push('/');
     },
     onError: (error: AxiosError) => {
-      console.log({ error });
-
       alert(
         error.message + '\n' + '\n' + (error.response?.data as any).message
       );

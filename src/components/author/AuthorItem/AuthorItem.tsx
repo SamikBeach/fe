9394565574d317470b062583bb41ccd6635c7a08
bong <jungcome7@gmail.com>
2024-08-30 14:53:02 +0900

    feat: add useProgressRouter

--- src/components/author/AuthorItem/AuthorItem.tsx
+++ src/components/author/AuthorItem/AuthorItem.tsx
@@ -1,29 +1,30 @@
 import { AuthorServerModel } from '@models/author';
-import Link from 'next/link';
 import { css } from 'styled-system/css';
 import { HStack } from 'styled-system/jsx';
 import AuthorItemInner from './AuthorItemInner';
+import useProgressRouter from '@hooks/useProgressRouter';
 
 interface Props {
   author: AuthorServerModel;
 }
 
 export default function AuthorItem({ author }: Props) {
+  const router = useProgressRouter();
+
   return (
-    <Link href={`/author/${author.id}`}>
-      <HStack
-        border="1px solid"
-        borderColor="gray.200"
-        padding="16px"
-        borderRadius="8px"
-        width="386px"
-        height="130px"
-        className={css({ cursor: 'pointer' })}
-        _hover={{ scale: 1.02, bgColor: 'gray.50' }}
-        transition="scale 0.1s ease-in-out"
-      >
-        <AuthorItemInner author={author} />
-      </HStack>
-    </Link>
+    <HStack
+      border="1px solid"
+      borderColor="gray.200"
+      padding="16px"
+      borderRadius="8px"
+      width="386px"
+      height="130px"
+      className={css({ cursor: 'pointer' })}
+      _hover={{ scale: 1.02, bgColor: 'gray.50' }}
+      transition="scale 0.1s ease-in-out"
+      onClick={() => router.push(`/author/${author.id}`)}
+    >
+      <AuthorItemInner author={author} />
+    </HStack>
   );
 }
---
+++ src/hooks/useProgressRouter.ts
@@ -0,0 +1,15 @@
+import { useRouter as useBaseRouter } from 'next/navigation';
+import NProgress from 'nprogress';
+
+export default function useRouter() {
+  const router = useBaseRouter();
+
+  const { push } = router;
+
+  router.push = async (...args: Parameters<typeof push>) => {
+    NProgress.start();
+    return push(...args);
+  };
+
+  return router;
+}

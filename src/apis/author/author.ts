e084905ebaa0502c479183f4f253352a44994e09
bong <jungcome7@gmail.com>
2024-09-02 14:18:48 +0900

    fix

--- src/apis/author/author.ts
+++ src/apis/author/author.ts
@@ -54,7 +54,6 @@ export function searchAuthors({
       search: keyword === '' ? undefined : keyword,
       ['filter.era_id']: eraId,
       sortBy: getSortBy(sort),
-      searchBy: ['title', 'title_in_eng', 'title_in_kor', 'author.name'],
       page,
       limit,
     },

80808b1aa8e7ecd91c4e6c717f7000e4230393a3
bong <jungcome7@gmail.com>
2024-09-30 23:07:10 +0900

    feat: OriginalWorks list UI details

--- src/components/original-work/OriginalWorkItem/OriginalWorkItemInner.tsx
+++ src/components/original-work/OriginalWorkItem/OriginalWorkItemInner.tsx
@@ -3,8 +3,9 @@ import { OriginalWorkServerModel } from '@models/original-work';
 import { ChatBubbleIcon, HeartFilledIcon } from '@radix-ui/react-icons';
 import { Avatar, Tooltip, Text } from '@radix-ui/themes';
 import { getPublicationDateText } from '@utils/original-work';
-import { useLocale, useTranslations } from 'next-intl';
+import { useLocale } from 'next-intl';
 import Link from 'next/link';
+import { GiBlackBook, GiSecretBook } from 'react-icons/gi';
 import { css } from 'styled-system/css';
 import { HStack, VStack } from 'styled-system/jsx';
 
@@ -13,8 +14,6 @@ interface Props {
 }
 
 export default function OringinalWorkItemInner({ originalWork }: Props) {
-  const t = useTranslations('OriginalWork');
-
   const locale = useLocale();
 
   const {
@@ -30,20 +29,39 @@ export default function OringinalWorkItemInner({ originalWork }: Props) {
   return (
     <HStack gap="20px">
       <Link href={`/original-work/${originalWork.id}`}>
-        <Avatar fallback={title[0]} size="7" />
+        <Avatar
+          radius="full"
+          fallback={
+            <GiSecretBook
+              className={css({
+                display: 'inline',
+                cursor: 'pointer',
+                color: 'gray.500',
+              })}
+              size="30px"
+            />
+          }
+          size="5"
+        />
       </Link>
 
-      <VStack alignItems="start" gap="0">
-        <VStack alignItems="start" gap="0">
-          <Tooltip content={title_in_kor}>
+      <VStack alignItems="start" justify="space-between">
+        <VStack alignItems="start" gap="2px">
+          <Tooltip content={`${title_in_kor} - ${title_in_eng}`}>
             <Link
               href={`/original-work/${originalWork.id}`}
               className={css({
                 textOverflow: 'ellipsis',
                 whiteSpace: 'nowrap',
                 overflow: 'hidden',
-                maxWidth: '240px',
+                maxWidth: '270px',
                 lineHeight: '19px',
+                color: 'gray.500',
+
+                cursor: 'pointer',
+                _hover: {
+                  textDecoration: 'underline',
+                },
               })}
             >
               <Text
@@ -51,39 +69,15 @@ export default function OringinalWorkItemInner({ originalWork }: Props) {
                 weight="bold"
                 className={css({
                   lineHeight: '19px',
-                  cursor: 'pointer',
-                  _hover: {
-                    textDecoration: 'underline',
-                  },
+                  color: 'black',
                 })}
               >
                 {locale === 'ko' ? title_in_kor : title}
-              </Text>
-            </Link>
-          </Tooltip>
-
-          <Tooltip content={title_in_eng}>
-            <Link
-              href={`/original-work/${originalWork.id}`}
-              className={css({
-                textOverflow: 'ellipsis',
-                whiteSpace: 'nowrap',
-                overflow: 'hidden',
-                maxWidth: '240px',
-                lineHeight: '17px',
-              })}
-            >
+              </Text>{' '}
               <Text
                 size="2"
-                weight="medium"
                 className={css({
-                  color: 'gray.700',
-                  lineHeight: '17px',
-
-                  cursor: 'pointer',
-                  _hover: {
-                    textDecoration: 'underline',
-                  },
+                  fontSize: '13px',
                 })}
               >
                 {title_in_eng}
@@ -91,7 +85,7 @@ export default function OringinalWorkItemInner({ originalWork }: Props) {
             </Link>
           </Tooltip>
 
-          {locale === 'ko' && (
+          {/* {locale === 'ko' && (
             <Tooltip content={title}>
               <Link
                 href={`/original-work/${originalWork.id}`}
@@ -120,35 +114,57 @@ export default function OringinalWorkItemInner({ originalWork }: Props) {
                 </Text>
               </Link>
             </Tooltip>
-          )}
+          )} */}
 
-          <Text size="2" color="gray">
+          <Text size="1" color="gray">
             {getPublicationDateText({ originalWork, locale })}
           </Text>
+
+          <AuthorAvatar
+            author={author}
+            withName
+            size="1"
+            textProps={{ size: '1', color: 'gray' }}
+          />
         </VStack>
 
-        <AuthorAvatar
-          author={author}
-          withName
-          size="1"
-          textProps={{ size: '1', color: 'gray' }}
-        />
-
-        <HStack gap="8px">
-          <Text size="2" color="gray">
-            {t('editions')} {edition_count}
-          </Text>
+        <HStack gap="10px" width="100%" alignItems="start">
           <HStack gap="3px">
+            <GiBlackBook
+              className={css({
+                display: 'inline',
+                cursor: 'pointer',
+                color: 'gray.500',
+              })}
+              size="18px"
+            />
+            <Text size="2" color="gray">
+              {edition_count}
+            </Text>
+          </HStack>
+          <HStack gap="3px">
+            <HeartFilledIcon
+              width="16px"
+              height="16px"
+              className={css({
+                color: 'gray.500',
+              })}
+            />
             <Text size="2" color="gray">
               {like_count}
             </Text>
-            <HeartFilledIcon color="gray" />
           </HStack>
           <HStack gap="3px">
+            <ChatBubbleIcon
+              width="16px"
+              height="16px"
+              className={css({
+                color: 'gray.500',
+              })}
+            />
             <Text size="2" color="gray">
               {comment_count}
             </Text>
-            <ChatBubbleIcon color="gray" />
           </HStack>
         </HStack>
       </VStack>
